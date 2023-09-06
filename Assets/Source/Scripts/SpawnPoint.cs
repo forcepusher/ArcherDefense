287296a5664c10dc335e8940c28a7f698a882959
forcepusher <bionitsoup@gmail.com>
2023-09-06 20:11:15 +0700

    Moving soldiers

---
+++ Assets/Source/Scripts/SpawnPoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    private const float SpawnInterval = 3f;
+
+    private float _timeSinceLastSpawn = 0;
+
+    [SerializeField]
+    private GameObject _spawnPrefab;
+
+    private void Update()
+    {
+        _timeSinceLastSpawn += Time.deltaTime;
+        if (_timeSinceLastSpawn > SpawnInterval)
+        {
+            Instantiate(_spawnPrefab, transform.position, transform.rotation);
+            _timeSinceLastSpawn = 0f;
+        }
+    }
+}
